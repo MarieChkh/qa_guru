4b88914826ea600fb6d841694632e4b85a9e6ac7
Marie Ch <zima04@mail.ru>
2025-03-16 23:20:59 +0300

    commit 3

--- project/src/main/java/guru/qa/Main.java
+++ project/src/main/java/guru/qa/Main.java
@@ -2,6 +2,6 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World and qa!");
+        System.out.println("!1111Hello, World and qa!");
     }
 }
